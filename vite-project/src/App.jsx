03d137749d9c153ca79e4a3ec4a96a34a3493be5
Lakshmi N <161922984+lakshu2702@users.noreply.github.com>
2024-12-08 12:34:53 +0530

    Initial commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import MainContent from "./components/MainContent"
+import Main from "./components/MainContent"
+import Navbar from "./components/Navbar"
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <MainContent />
+    </>
+  )
+}
